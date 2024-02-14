c41a7e49baac15bee9fae665df66fb703f7b7782
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2024-02-14 18:32:13 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'FormulariosReactivos';
+export class AppComponent implements OnInit{
+
+  dynamicForm! : FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.dynamicForm = this.formBuilder.group({
+      numberOfTickets: ['', Validators.required],
+      tickets: new FormArray([])
+    });
+  }
+
+
 }
