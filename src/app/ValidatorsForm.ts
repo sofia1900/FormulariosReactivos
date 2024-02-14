9db0d59e0f2d4f4652b090a18900603324eb056b
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2024-02-14 18:09:44 +0100

    .

--- src/app/ValidatorsForm.ts
+++ src/app/ValidatorsForm.ts
@@ -2,8 +2,9 @@ import {AbstractControl, ValidationErrors} from "@angular/forms";
 
 export class ValidatorsForm {
   static match (control : AbstractControl) : ValidationErrors | null {
-    let pas = control.value
-    if (pas == pas2){
+    let password = control.value
+    let passwordConfirm = control.value
+    if (password == passwordConfirm){
       return null
     }else{
       return {match : true}
