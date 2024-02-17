b2bf15c26ebd2ab88abecd7d062006c2e26888f4
Sofia <sofia.jimlap@educa.jcyl.es>
2024-02-17 13:49:48 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,50 @@ import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class AppComponent implements OnInit{
 
-  dynamicForm! : FormGroup;
-  submitted = false;
+  formulario! : FormGroup;
+  numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.dynamicForm = this.formBuilder.group({
-      numberOfTickets: ['', Validators.required],
-      tickets: new FormArray([])
-    });
+    this.formulario =  this.formBuilder.group({
+      numberOfTickets : ['', [Validators.required]],
+      tickets : new FormArray([])
+    })
   }
 
+  onNumeroTicketsChange() {
+    const numTickets = this.formulario.get('numberOfTickets')?.value
 
+    this.ticketArray.clear()
+
+    for (let i = 0; i < numTickets; i++) {
+      this.ticketArray.push(
+        this.formBuilder.group({
+          nombre: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]],
+        })
+      )
+    }
+  }
+
+  get ticketArray() { return this.formulario.get('tickets') as FormArray }
+  get ticketFormGroups() { return this.ticketArray.controls as FormGroup[]; }
+
+  enviar(){
+    if (this.formulario.valid){
+      alert("ENVIADO")
+      this.formulario.reset()
+      this.ticketArray.clear()
+    }
+  }
+
+  resert(){
+    this.formulario.reset()
+    this.ticketArray.clear()
+  }
+
+  clear(){
+    this.ticketArray.reset()
+  }
 }
